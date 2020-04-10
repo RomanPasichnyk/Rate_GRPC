64a0eebce542d88b9cc9857e6b08eb8a12ae8641
roman.pasichnyk <rom.pasichnyk@gmail.com>
2020-04-10 15:53:58 +0300

    refactoring code.

--- src/main/java/com/rate/providers/CurrencyProvider.java
+++ src/main/java/com/rate/providers/CurrencyProvider.java
@@ -14,6 +14,10 @@ public class CurrencyProvider {
 
     private static final float RATE_UAH = 24.57f;
 
+    private static final float COEFFICIENT = 0.3f;
+
+    private static final float LOW_VALUE = 0.001f;
+
     private final Map<Currency, Float> rateToUsd = new HashMap<>();
 
     private final Map<String, Float> bankRates = new HashMap<>();
@@ -38,8 +42,21 @@ public class CurrencyProvider {
     }
 
     public void initBankRates(float value) {
-        bankRates.put("UKRSIB", value * new Random().nextFloat());
-        bankRates.put("PRIVAT", value * new Random().nextFloat());
+        bankRates.put("UKRSIB", value * generateRandomValue());
+        bankRates.put("PRIVAT", value * generateRandomValue());
+    }
+
+    /**
+     * Generate value between 0.001 and 0.3.
+     *
+     * @return some value.
+     */
+    public float generateRandomValue() {
+        float value = new Random().nextFloat() + LOW_VALUE;
+        if (value > COEFFICIENT) {
+            value = COEFFICIENT;
+        }
+        return value;
     }
 
     /**
